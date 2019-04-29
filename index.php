9ec12016fe979d0de613f33a10e4bf90d16bef00
Finnko <finko85@gmail.com>
2019-04-30 01:06:52 +0300

    add lesson2 task2

--- index.php
+++ index.php
@@ -8,19 +8,73 @@ $title = 'PHP урок 2';
     <title><?= $title ?></title>
 </head>
 <body>
-   <div class="task1">
-       <?php
-       $a = -5;
-       $b = 6;
+<div class="task1">
+<!--       --><?php
+//       $a = -5;
+//       $b = 6;
+//
+//       if ($a >= 0 && $b >= 0) {
+//           echo $a - $b;
+//       } else if ($a < 0 && $b < 0) {
+//           echo $a * $b;
+//       } else {
+//           echo $a + $b;
+//       }
+//       ?>
+</div>
+<div class="task2">
+    <?php
+    $c = rand(0, 15);
+    echo 'Переменная c=' . $c . '</br>';
+    switch ($c) {
 
-       if ($a >= 0 && $b >= 0) {
-           echo $a - $b;
-       } else if ($a < 0 && $b < 0) {
-           echo $a * $b;
-       } else {
-           echo $a + $b;
-       }
-       ?>
-   </div>
+        case 1:
+        echo 1;
+
+        case 2:
+        echo 2;
+
+        case 3:
+        echo 3;
+
+        case 4:
+        echo 4;
+
+        case 5:
+        echo 5;
+
+        case 6:
+        echo 6;
+
+        case 7:
+        echo 7;
+
+        case 8:
+        echo 8;
+
+        case 9:
+        echo 9;
+
+        case 10:
+        echo 10;
+
+        case 11:
+        echo 11;
+
+        case 12:
+        echo 12;
+
+        case 13:
+        echo 13;
+
+        case 14:
+        echo 14;
+
+        case 15:
+        echo 15;
+        break;
+    }
+    ?>
+</div>
 </body>
 </html>
