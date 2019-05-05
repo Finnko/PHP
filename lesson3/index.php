e9437bfba088f6c69484067cadce4a8b12d96fa8
Finnko <finko85@gmail.com>
2019-05-05 23:05:44 +0300

    lesson 3 add task 1

--- lesson3/index.php
+++ lesson3/index.php
@@ -20,5 +20,24 @@ $title = 'PHP урок 3';
     }
     ?>
 </div>
+<div class="task2">
+    <?php
+    $i = 0;
+
+    do {
+        if ($i == 0) {
+            echo $i . ' - это ноль' . '<br>';
+        } elseif ($i % 2 == 0) {
+            echo $i . ' - это четное число' . '<br>';
+        } else {
+            echo $i . ' - это нечетное число' . '<br>';
+        }
+
+        $i++;
+
+    } while ($i <= 10);
+
+    ?>
+</div>
 </body>
 </html>
