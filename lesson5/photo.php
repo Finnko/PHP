944fadfd7358111466dcd38c520047e02465e32a
Alex <finko85@gmail.com>
2019-05-17 12:14:34 +0300

    lesson 5 tasks done

---
+++ lesson5/photo.php
@@ -0,0 +1,29 @@
+<?php
+
+$link = mysqli_connect(
+    'localhost',
+    'root',
+    'root1234',
+    'gbphp',
+    '8888'
+);
+
+if (!empty($_GET['id'])) {
+    $id = (int)$_GET['id'];
+
+    $sql = "SELECT url, name, count FROM gallery WHERE id = $id";
+    mysqli_query($link, "UPDATE gallery SET count = count + 1 WHERE id = $id");
+
+    $res = mysqli_query($link, $sql);
+}
+
+$row = mysqli_fetch_assoc($res);
+
+$html = <<<php
+    <div class="fullSize">
+        <img src="{$row['url']}" alt="{$row['name']}">
+        <p>Количество просмотров равно {$row['count']}</p>
+    </div>
+php;
+
+echo $html;
